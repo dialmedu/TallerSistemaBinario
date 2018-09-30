afb24664cf086442862b8b78198cc6b3361fcb6b
Unknown <alfresar000@gmail.com>
2018-09-30 11:40:02 -0500

    Metodos
    Sumar, decimal y hexadecimal

--- src/ufps/logicaNegocio/SistemaBinario.java
+++ src/ufps/logicaNegocio/SistemaBinario.java
@@ -1,5 +1,7 @@
 package ufps.logicaNegocio;
 
+import java.util.*;
+
 public class SistemaBinario implements OperacionBinario {
 
     private int i;
@@ -54,15 +56,57 @@ public class SistemaBinario implements OperacionBinario {
         }
         return maxBinario[maxRepeticiones];
     }
-
-    @Override
+  
     public Binario getSumaTotal() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Binario getResta(int indiceBinario1, int indiceBinario2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String cad2= String.valueOf(indiceBinario1);;
+        String cad3= String.valueOf(indiceBinario2);
+        int cadena[]= new int[20];
+        int cdena[]= new int[20];
+        int cdenamd[]= new int[20];
+        int result[]= new int[20];
+        int i, dim=0, dim2=0;
+        //resta
+        System.out.println("");
+        int acarreo2=0;
+        int result2[]= new int[20];
+        for (i =dim-1; i >= 0; i--) {
+            if (acarreo2==1) {
+                cadena[i]= cadena[i]- acarreo2;
+                if (cadena[i]==0) {
+                    cadena[i]=0;
+                } else{
+                cadena[i]=1; }
+            }
+            result2[i+1]= cadena[i]- cdenamd[i];
+            if (result2[i+1]== -1) {
+                acarreo2=1;
+                result2[i+1]=1;
+            }else{
+                if (result2[i+1]==1) {
+                    acarreo2=0;
+                    result2[i]=1;
+                } else{
+                    if (result2[i+1]== 0) {
+                        acarreo2=0;
+                        result2[i]=0;
+                    }
+                }
+            }
+        }
+        String suma2="";
+        if (acarreo2==1) 
+            result2[0]=acarreo2;
+        System.out.println("resta: ");
+            for (int k = 0; k < dim+1; k++) {
+                //suma2 += System.out.print(result2[k]+" ");
+                suma2+= String.valueOf(result2[k]+" ");
+            }
+        return new Binario(suma2);    
     }
 
     @Override
@@ -72,20 +116,172 @@ public class SistemaBinario implements OperacionBinario {
 
     @Override
     public long getDecimal(int indice_Del_Binario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //binario a decimal
+        int  cifra, resultado=0, exp=0;
+        int n;
+         Scanner dato= new Scanner(System.in);
+        n= dato.nextInt();
+        do {
+            cifra= n % 10;
+            resultado= resultado+ cifra* (int)Math.pow(2,exp);
+            exp++;
+            n=n/10;
+        } while (n>0);
+        System.out.println("num decimal: "+resultado);
+        return resultado;
     }
 
     @Override
     public Binario getSuma(int indiceBinario1, int indiceBinario2) {
-        /**
-         * example: 
-         */
-        return new Binario();
+        /** String cad2= new String();
+        String cad3= new String();
+        String cad2= String.valueOf(indiceBinario1);
+        String cad3= String.valueOf(indiceBinario2); **/
+        String cad2= "1011";
+        String cad3= "101";
+        int cadena[]= new int[20];
+        int cdena[]= new int[20];
+        int cdenamd[]= new int[20];
+        int result[]= new int[20];
+        int i, dim, dim2;
+        
+        Scanner dato= new Scanner(System.in);
+        System.out.println("suma");
+        System.out.println("n1");
+       // cad2= dato.nextLine();
+         cad2= cad2;
+         
+        for (i = 0; i < cad2.length(); i++) {
+            //transforma la cadena en un int
+            if (cad2.charAt(i) == 48){ 
+                cadena[i]=0;
+            }
+        else{
+               cadena[i]=1;    }
+    }
+        
+        dim= i;
+        System.out.println("n2");
+        //cad3= dato.nextLine();
+        cad3=cad3;
+        
+        for (i = 0; i < cad3.length(); i++) {
+            if (cad3.charAt(i) == 48) {
+                cdena[i]=0;
+            } else{
+               cdena[i]=1;    }
+    }
+        
+    dim2= i;
+    int j=0;
+        for (int k = 0; k < dim; k++) {
+            if (k<(dim-dim2)) {
+                cdenamd[k]=0;
+            }else{
+                cdenamd[k]=cdena[j];
+                j++;
+            }
+        }
+        System.out.println("numero 1:");
+        for (i = 0; i < dim; i++) {
+            System.out.print(cadena[i]+"  ");
+        }
+     System.out.println("");    
+    System.out.println("numero 2");
+        for (int l = 0; l < dim; l++) {
+            System.out.print(cdenamd[l]+"  ");
+        }
+    //suma    
+    System.out.println(dim);
+    int acarreo=0;
+        for (i=dim-1; i >= 0; i--) {
+            if (acarreo==1) {
+                cadena[i]= cadena[i]+acarreo;
+                if (cadena[i]==2) {
+                    acarreo=1;
+                    cadena[i]=0;
+                }
+            }
+            result[i+1]= cadena[i]+cdenamd[i];
+                if (result[i+1] == 2) {
+                    acarreo=1;
+                    result[i+1]=0;
+                }else{
+                    acarreo=0;
+                }   
+        }
+        
+        if (acarreo==1) 
+            result[0]= acarreo;
+        String suma="";
+             System.out.println("");
+             System.out.println("Suma:");
+            for (i= 0; i<dim+1; i++) {
+                //System.out.print(result[i]+" ");
+            suma+= String.valueOf(result[i]+" ");    
+            }
+            
+        return new Binario(suma);
     }
 
     @Override
     public String getHexaDecimal(int indice_Del_Binario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //binario a hexa
+       // Scanner dato= new Scanner(System.in);
+        int n, cont=0, aux, acum=0;
+        String resultado="";
+        System.out.println("num: ");
+        //n= dato.nextInt(); 
+        //n= String.valueOf(indice_Del_Binario);
+        n= 1001;
+        while(n>0){
+            acum=0;
+            cont=0;
+            while(cont<4){
+                aux= n%10;
+                if (aux ==1) {
+                    switch(cont){
+                        case 0:
+                            acum=acum+1;
+                            break;
+                        case 1:
+                            acum=acum+2;
+                            break;    
+                        case 2:
+                            acum=acum+4;
+                            break; 
+                        case 3:
+                            acum=acum+8;
+                            break;
+                        default:
+                            break;    
+                    }
+                }
+                
+                n= n/10;
+                cont++;
+            }
+            if (acum<=9) {
+                resultado= acum+resultado;
+            } else if(acum>9){
+               if(acum==10){
+                resultado= "A"+acum+resultado;
+               } else if(acum==11){
+                resultado= "B"+resultado;
+               } else if(acum==12){
+                resultado= "C"+resultado;
+               } else if(acum==13){
+                resultado= "D"+resultado;
+               }else if(acum==14){
+                resultado= "E"+resultado;
+               } else if(acum==15){
+                resultado= "F"+resultado;
+               }
+            }
+        }
+         resultado= " "+resultado; 
+         //System.out.println("Resultado: "+resultado);
+        return resultado+= "Resultado: "+resultado;
     }
 
     @Override
